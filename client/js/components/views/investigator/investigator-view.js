807227e760833ea29185ce07babdd1301800ad66
GLOBANT\bankim.sutaria <bankim.sutaria@globant.com>
2017-02-08 16:10:25 +0530

    update investigator view table json structure

--- client/js/components/views/investigator/investigator-view.js
+++ client/js/components/views/investigator/investigator-view.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import {MainViewTpl, MainViewHeader} from 'js/components/base-views/main-view';
 import CustomTable from 'js/components/table-component/custom-table';
-import Gender from 'js/components/table-component/custom-gender';
+import CustomType from 'js/components/table-component/custom-type';
+import CustomEntity from 'js/components/table-component/custom-entity';
 
 import CustomSlider from 'js/components/range-slider-component/custom-range-slider';
 
@@ -21,44 +22,15 @@ export default class InvestigatorView extends React.Component{
     super();
     this.state = {
       "tableData" : {
-            "title":"Custom Table React Template",
-            "columnDetail": [{"title":"Name","isRequired":true,"sortType":"content", "defaultSort":"asc"},{"title":"Location","isRequired":true,"sortType":"content","defaultSort":"asc"},{"title":"Age","isRequired":true,"sortType":"content","defaultSort":"asc"},{"title":"Sex","isRequired":true,"customComponent":Gender,"sortType":"custom_content","defaultSort":"asc","sortField":"gender"},{"title":"Birthdate","isRequired":true,"sortType":"date","defaultSort":"asc"}],
+            "title":"Entity Results",
+            "columnDetail": [{"title":"Entities","isRequired":true,"customComponent":CustomEntity,"sortType":"custom_content","defaultSort":"asc","sortField":"entityTitle"},{"title":"Behavior","isRequired":true,"sortType":"content","defaultSort":"asc"},{"title":"Type","isRequired":true,"customComponent":CustomType},{"title":"Score","isRequired":true,"sortType":"date","defaultSort":"asc"}],
             "tableContent": [
-                {"Location":"Mumbai","Age":"28","Name":"Vikram","Sex":{"gender":"Male","isImage":true},"Birthdate":"February 24, 1988"},
-                {"Name":"Bankim S","Sex":{"gender":"Male","isImage":true},"Location":"Pune","Age":"28","Birthdate":"February 24, 1988"},
-                {"Name":"Pooja","Age":"26","Location":"Surat","Sex":{"gender":"Female","isImage":true},"Birthdate":"March 11, 1988"},
-                {"Name":"Sivaprasad Raju","Age":"30","Location":"Pune","Sex":{"gender":"Male","isImage":true},"Birthdate":"June 11, 1995"},
-                {"Name":"Alessia","Age":"25","Location":"Delhi","Sex":{"gender":"Female","isImage":true},"Birthdate":"July 11, 1995"},
-                {"Name":"Abhimanyu","Age":"29","Location":"Hydrabad","Sex":{"gender":"Male","isImage":true},"Birthdate":"September 15, 1985"},
-                {"Name":"Danica","Age":"32","Location":"Goa","Sex":{"gender":"Female","isImage":true},"Birthdate":"September 11, 1985"}
+                {"Entities":{"entityType":"system","entityTitle":"system@authority"},"Behavior":"System Activity","Type":{},"Score":"10"},
+                {"Entities":{"entityType":"system","entityTitle":"system1@authority"},"Behavior":"System Activity1","Type":{},"Score":"7"},
+                {"Entities":{"entityType":"user","entityTitle":"user@authority"},"Behavior":"User Activity","Type":{},"Score":"6"},
+                {"Entities":{"entityType":"user","entityTitle":"user2@authority"},"Behavior":"User2 Activity","Type":{},"Score":"7.2"}
             ]
         },
-
-      "accordionData" :{
-        "panels": [
-          {
-            "header" : "Collapsible Group Item #1",
-            "content": "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.",
-            "eventKey": "1",
-            "componentUsed": "NA", // false or component instance
-            "componentData": {} // used component's data
-          },
-          {
-            "header" : "Collapsible Group Item #2",
-            "content": "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.",
-            "eventKey": "2",
-            "componentUsed": Gender, // false or component instance
-            "componentData": {"gender":"Male","isImage":true} // used component's data
-          },
-          {
-            "header" : "Collapsible Group Item #3",
-            "content": "Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.",
-            "eventKey": "3",
-            "componentUsed": Gender, // false or component instance
-            "componentData": {"gender":"Female","isImage":true} // used component's data
-          }
-        ]
-      },
 
       "expandPanelStatus": "close",  
       "tabContainerStatus": "close",  
