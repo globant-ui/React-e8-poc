021dbb14e47b43d5538d5e9f737e237f2fcba844
GLOBANT\bankim.sutaria <bankim.sutaria@globant.com>
2017-02-08 16:08:55 +0530

    Add Entity and type sub component

---
+++ client/js/components/table-component/custom-entity.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import * as styles from '!style!css!stylus!./custom-entity.styl';
+
+class CustomEntity extends React.Component{
+    render(){
+        const imgBlock = (this.props.data.entityType).toLowerCase() === "system" ? <span className='icon-system'/> : <span className='icon-user'/>;
+        return <div style={{display:'table'}}>{imgBlock}<span className={'block-txt'}>{this.props.entityTitle}</span></div>;
+    }
+}
+
+export default CustomEntity;
---
+++ client/js/components/table-component/custom-type.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import * as styles from '!style!css!stylus!./custom-type.styl';
+
+class CustomType extends React.Component{
+    render(){
+        return <span className={'icon-type'}/>;
+    }
+}
+
+export default CustomType;
