1133dca07677b8cdd9bb4f5ab80ae2073678aa1e
GLOBANT\bankim.sutaria <bankim.sutaria@globant.com>
2017-02-10 10:52:19 +0530

    Remove type image reference

--- client/js/components/table-component/custom-type.js
+++ client/js/components/table-component/custom-type.js
@@ -3,7 +3,7 @@ import * as styles from '!style!css!stylus!./custom-type.styl';
 
 class CustomType extends React.Component{
     render(){
-        return <div className={'custom-type-component'}><span className={'icon-type'}/></div>;
+        return <div className={'custom-type-component'}><span className={'icon-type fa fa-link fa-lg'}/></div>;
     }
 }
 
