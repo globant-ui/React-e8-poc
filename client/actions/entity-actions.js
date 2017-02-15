92e5dec735e5209a0d24dff020446b850314ef2b
GLOBANT\bankim.sutaria <bankim.sutaria@globant.com>
2017-02-15 13:51:17 +0530

    Update fetchEntityData function

--- client/actions/entity-actions.js
+++ client/actions/entity-actions.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 import store from '../store';
 
-export function fetchEntityData(){
-    store.dispatch({
-        type: "FETCH_ENTITY_DATA",
-        payload: axios.get("https://api.myjson.com/bins/xc7yp")
+export function fetchEntityData(p_callback){
+    axios.get('https://api.myjson.com/bins/xc7yp')
+    .then(p_callback)
+    .catch(function (error) {
+      console.log(error);
     });
 }
 export function fetchEntityDataWithParams(obj){
